ce58fbf65eb1ced2d1a08778cd165ff6290801ec
Stanislav Vozikov <stanislav.vozikov@gmail.com>
2018-08-30 10:27:03 +0300

    add: подключение babel к webpack 4

--- src/test.js
+++ src/test.js
@@ -1,5 +1,12 @@
 function test() {
     console.log('Все работает')
+
+    let string = ''
+    let number = string === 'строка'
+
+    number
+        ? console.log('равно')
+        : console.log('не равно')
 }
 
 export default test
